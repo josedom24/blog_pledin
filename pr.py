29c76b5494c5da2effcb2f34094f636e02df4155
José Domingo Muñoz <josedom24@gmail.com>
2022-02-08 14:41:07 +0100

    ic

---
+++ pr.py
@@ -0,0 +1,10 @@
+import json,subprocess
+a=subprocess.check_output(["gh","pr","list","--json","number"])
+d=json.loads(a.decode('utf-8'))
+for number in d:
+    a=subprocess.check_output(["gh","pr","view",str(number.get("number")),"--json","body"])
+    dd=json.loads(a.decode('utf-8'))
+    lista=["pepe"]
+    for user in lista:
+        if user in dd.get("body"):
+            print("ok")
