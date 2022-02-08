79e62142327651c861da893bbae0773eefb5f035
José Domingo Muñoz <josedom24@gmail.com>
2022-02-08 14:55:00 +0100

    ic

--- pr.py
+++ pr.py
@@ -1,10 +1,10 @@
 import json,subprocess,os
+lista=["zithromax"]
 a=subprocess.check_output(["gh","pr","list","--json","number"])
 d=json.loads(a.decode('utf-8'))
 for number in d:
     a=subprocess.check_output(["gh","pr","view",str(number.get("number")),"--json","body"])
     dd=json.loads(a.decode('utf-8'))
-    lista=["pepe"]
     for user in lista:
         if user in dd.get("body"):
             os.system("gh pr close -d "+str(number.get("number")))
